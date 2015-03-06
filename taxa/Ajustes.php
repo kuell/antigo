c86c1006190298b5fdab188adb44beb0e666c571
Tiago Correa <tiagocorrea_442@hotmail.com>
2015-03-06 09:43:59 -0400

    Inclusão da ferramenta Ajustes no modulo Taxas

--- taxa/Ajustes.php
+++ taxa/Ajustes.php
@@ -4,7 +4,7 @@ require ('../class/Corretor.class.php');
 $corretor = new Corretor($_GET['cor']);
 
 $datai = implode('-', array_reverse(explode('/', $_GET['datai'])));
-$dataf = implode('-', array_reverse(explode('/', $_GET['datai'])));
+$dataf = implode('-', array_reverse(explode('/', $_GET['dataf'])));
 
 $dados = $corretor->getTaxaAjustes($datai, $dataf);
 
@@ -56,49 +56,49 @@ $dados = $corretor->getTaxaAjustes($datai, $dataf);
 	<div class="container-fluid">
 		<div class="row col-md-12">
 <?php foreach ($dados as $data => $val) {?>
-									<div class="well">
+											<div class="well">
 
-										<div class="label-danger"><?php echo $data;?></div>
+												<div class="label-danger"><?php echo $data;?></div>
 
 	<?php foreach ($val as $grupo => $v) {?>
-															<div class="label-info" style="text-align: center"><?php echo $grupo;?></div>
-															<table class="table table-bordered">
-																<thead>
-																	<tr>
-																		<th>Descrição</th>
-																		<th>Qtde.</th>
-																		<th>Peso</th>
-																		<th>Valor</th>
-																		<th>Tipo</th>
-																		<th></th>
-																	</tr>
-																</thead>
-																<tbody>
-		<?php foreach ($v as $item) {?>
+																			<div class="label-info" style="text-align: center"><?php echo $grupo;?></div>
+																			<table class="table table-bordered">
+																				<thead>
 																					<tr>
-																						<td><?php echo $item->descricao;?></td>
-																						<td><input type="text" id="qtd<?php echo $item->id;?>" class="form-control int" value="<?php echo $item->qtd;?>"></td>
-																						<td><input type="text" id="peso<?php echo $item->id;?>" class="form-control qtd" value="<?php echo $item->peso;?>"></td>
-																						<td><input type="text" id="valor<?php echo $item->id;?>" class="form-control valor" value="<?php echo $item->valor;?>"></td>
-																						<td>
-																							<select id="tipo<?php echo $item->id;?>" class="form-control">
-																								<option value="c" <?php if ($item->tipo == 'c') {echo 'selected';
+																						<th>Descrição</th>
+																						<th>Qtde.</th>
+																						<th>Peso</th>
+																						<th>Valor</th>
+																						<th>Tipo</th>
+																						<th></th>
+																					</tr>
+																				</thead>
+																				<tbody>
+		<?php foreach ($v as $item) {?>
+																											<tr>
+																												<td><?php echo $item->descricao;?></td>
+																												<td><input type="text" id="qtd<?php echo $item->id;?>" class="form-control int" value="<?php echo $item->qtd;?>"></td>
+																												<td><input type="text" id="peso<?php echo $item->id;?>" class="form-control qtd" value="<?php echo $item->peso;?>"></td>
+																												<td><input type="text" id="valor<?php echo $item->id;?>" class="form-control valor" value="<?php echo $item->valor;?>"></td>
+																												<td>
+																													<select id="tipo<?php echo $item->id;?>" class="form-control">
+																														<option value="c" <?php if ($item->tipo == 'c') {echo 'selected';
 			}
 			?>>A PAGAR</option>
-																								<option value="d" <?php if ($item->tipo == 'd') {echo 'selected';
+																														<option value="d" <?php if ($item->tipo == 'd') {echo 'selected';
 			}
 			?>>A RECEBER</option>
-																								<option value="i" <?php if ($item->tipo == 'i') {echo 'selected';
+																														<option value="i" <?php if ($item->tipo == 'i') {echo 'selected';
 			}
 			?>>INFORMATIVO</option>
-																							</select>
-																						<td>
-																							<button class="btn btn-primary" onclick='gravar(<?php echo $item->id;?>)'>Ajustar</button>
-																						</td>
-																					</tr>
+																													</select>
+																												<td>
+																													<button class="btn btn-primary" onclick='gravar(<?php echo $item->id;?>)'>Ajustar</button>
+																												</td>
+																											</tr>
 			<?php }?>
 		</tbody>
-															</table>
+																			</table>
 		<?php }?>
 	</div>
 	<?php }?>
