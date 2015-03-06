049ff49b168b7d76723d77fb3e4523c03917c0e1
Tiago Correa <tiagocorrea_442@hotmail.com>
2015-03-06 12:38:26 -0400

    Alteração da tela principal do Gerencial

--- gerencial/principal.php
+++ gerencial/principal.php
@@ -3,16 +3,15 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Untitled Document</title>
-<link href="../includes/skins/mxkollection3.css" rel="stylesheet" type="text/css" />
+<link href="../css/bootstrap.css" rel="stylesheet" type="text/css" />
+<link href="../css/calendario.css" rel="stylesheet" type="text/css" />
 <script src="../js/jquery.min.js" language="javascript"></script>
-<script src="../bibliotecas/mascara.js" language="javascript"></script>
+<script src="../js/jquery.ui.js" type="text/javascript"></script>
+<script src="../js/jquery.maskedinput.js" type="text/javascript"></script>
+<script src="../js/jquery.maskMoney.js" type="text/javascript"></script>
+<script src="../js/scripts.js" type="text/javascript"></script>
+
 <script type="text/javascript">
-	$(function (){
-				$(".data").mask('99-99-9999');
-				$("table").css("width","400px");
-				$("table th").css("font-size","20px");
-				$("button").css("font-size","20px");
-				})
 	function abre(pagina){
 		data1 = document.getElementById('data1').value
 		data2 = document.getElementById('data2').value
@@ -34,84 +33,88 @@
 </head>
 
 <body>
-<div class="acao_pagina">Relatorios Gerenciais</div>
-<form id="form1" name="form1" method="post" action="">
-  <table width="auto%" border="0" align="center" class="KT_tngtable">
-    <tr>
-      <th>Periodo</th>
-      <td rowspan="2"><label>
-        <input type="text" name="data1" id="data1" class="data" value="<?php echo date('01-m-Y')?>"/>
-      </label>
-        a
-        <label>
-          <input type="text" name="data2" id="data2" class="data" value="<?php echo date('d-m-Y')?>"/>
-        </label></td>
-    </tr>
+  <div class="well">
+      <h3>  Relatorios Gerenciais</h3>
+    </p>
+  </div>
 
-  </table>
-  <br />
-  <table border="0" align="center" class="KT_tngtable">
-    <tr class="acao_pagina">
-      <th colspan="2">Analise de Corretores</th>
-    </tr>
-    <tr>
-      <td><label>
-        <input type="button" name="Rendimento / Corretor" id="Rendimento / Corretor" value="Rendimento / Corretor" onclick="abre('rend_cor.php')" />
-      </label></td>
-      <td><input type="button" name="Rendimento / Corretor2" id="Rendimento / Corretor2" value="Balanço Fiscal / Corretor" onclick="abre('rend_balFiscal.php')" /></td>
-    </tr>
-    <tr>
-      <td><input type="button" name="Rendimento / Corretor3" id="Rendimento / Corretor3" value="Resultado / Corretor" onclick="abre('rend_resultado.php')" /></td>
-      <td>&nbsp;</td>
-    </tr>
-  </table>
-  <table border="0" align="center" class="KT_tngtable">
-    <tr class="acao_pagina">
-      <th colspan="3">Industria e Faturamento</th>
-      </tr>
-      <tr>
-      <td colspan="3">
-        Tipo de Arquivo:<select id='tipoArquivo'>
-                            <option value="pdf">PDF</option>
-                            <option value="xls">Excel</option>
-                        </select>
-      </td>
-    </tr>
-    <tr>
-      <td><input type="button" name="Rendimento / Corretor4" id="Rendimento / Corretor4" value="Analise de Rendimento" onclick="abre('ind_analiseRend.php')" /></td>
-      <td><input type="button" name="Custos de Produção" id="Custos de Produção" value="Custos de Produção  (mensal)" onclick="abreParametro('apuracao_custo_prod.php', 'tipoArquivo')" /></td>
-	  <td><input type="button" name="Custos de Produção" id="Custos de Produção" value="Custos de Produção  (diario)" onclick="abre('apuracao_custo_prod_diario.php')" /></td>
-    </tr>
-  </table>
-  <table border="0" align="center" class="KT_tngtable">
-    <tr class="acao_pagina">
-      <th colspan="4">Recursos Humanos</th>
-    </tr>
-    <tr>
-      <td><input type="button" name="Rendimento / Corretor5" id="Rendimento / Corretor5" value="Balanço RH" onclick="abre('rh_balanco_geral.php')" /></td>
-	  <td><input type="button" name="Quadro de Funcionarios" id="Quadro de Funcionarios" value="Quadro de Funcionarios" onclick="abre('rh/quadro_funcionarios.php')" /></td>
-	  <td><input type="button" name="matriz" id="matriz" value="Matriz do Balanço" onclick="abre('rh/matriz_balanco.php')" /></td>
-    <td>
-      <input type="button" name="analiseGlobal" id="analiseGlobal" value="Analise Global" onclick="abreParametro('rh/rh_balanco_analise_global.php', 'setor')" /><br />
-      <select id="setor">
-        <option value="">Selecione um setor ...</option>
+
+ <div class="col-md-9 well inverse">
+          <div class="col-md-1">
+            Periodo:
+            </div>
+            <div class="col-md-4">
+              <input type="text" name="data1" id="data1" class="form-control data" value="<?php echo date('01-m-Y')?>"/>
+            </div>
+            <div class="col-md-4">
+              <input type="text" name="data2" id="data2" class="form-control data" value="<?php echo date('d-m-Y')?>"/>
+            </div>
+
+ </div>
+ <div class="col-md-9 well">
+   <div class="col-md-12">
+     <h4>Analise de Corretores</h4>
+   </div>
+   <div class="col-md-12">
+    <input type="button" class="btn btn-primary btn-sm" name="Rendimento / Corretor" id="Rendimento / Corretor" value="Rendimento / Corretor" onclick="abre('rend_cor.php')" />
+    <input type="button" class="btn btn-info btn-sm" name="Rendimento / Corretor2" id="Rendimento / Corretor2" value="Balanço Fiscal / Corretor" onclick="abre('rend_balFiscal.php')" />
+    <input type="button" class="btn btn-danger btn-sm" name="Rendimento / Corretor3" id="Rendimento / Corretor3" value="Resultado / Corretor" onclick="abre('rend_resultado.php')" />
+   </div>
+ </div>
+
+<div class="col-md-9 well">
+   <div class="col-md-12">
+    <div class="col-md-5">
+      <h4>Industria e Faturamento</h4>
+    </div>
+    <div class="col-md-2">
+      Tipo de Arquivo:
+    </div>
+    <div class="col-md-2">
+      <select id='tipoArquivo' class="form-control">
+        <option value="pdf">PDF</option>
+        <option value="xls">Excel</option>
+      </select>
+    </div>
+   </div>
+   <div class="col-md-12">
+    <input type="button" class="btn btn-warning btn-sm" name="Rendimento / Corretor4" id="Rendimento / Corretor4" value="Analise de Rendimento" onclick="abre('ind_analiseRend.php')" />
+    <input type="button" class="btn btn-default btn-sm" name="Custos de Produção" id="Custos de Produção" value="Custos de Produção  (mensal)" onclick="abreParametro('apuracao_custo_prod.php', 'tipoArquivo')" />
+    <input type="button" class="btn btn-primary btn-sm" name="Custos de Produção" id="Custos de Produção" value="Custos de Produção  (diario)" onclick="abre('apuracao_custo_prod_diario.php')" />
+   </div>
+ </div>
+
+
+<div class="col-md-9 well">
+   <div class="col-md-12">
+    <div class="col-md-5">
+      <h4>Recursos Humanos</h4>
+    </div>
+    <div class="col-md-2">
+      Setor:
+    </div>
+    <div class="col-md-5">
+          <select id="setor" class="form-control">
+              <option value="">Selecione um setor ...</option>
 <?php
 $conn = new mysqli('localhost', 'root', 'aporedux', 'sig');
 $sql  = "select * from setor where rh = 1";
 $qr   = $conn->query($sql) or die('Erro na obtensao dos dados'.$sql);
 while ($res = $qr->fetch_object()) {
 	?>
-															   <option value="<?php echo $res->id_setor;?>"><?php echo $res->setor;
+																			                    <option value="<?php echo utf8_decode($res->id_setor);?>"><?php echo $res->setor;
 	?></option>
-	<?php
-}
-?>
+	<?php }?>
       </select>
-    </td>
-    </tr>
-  </table>
-  <p></p>
+    </div>
+   </div>
+   <div class="col-md-12">
+    <input type="button" class="btn btn-info btn-info" name="matriz" id="matriz" value="Matriz do Balanço" onclick="abre('rh/matriz_balanco.php')" />
+    <input type="button" class="btn btn-default btn-sm" name="Rendimento / Corretor5" id="Rendimento / Corretor5" value="Balanço RH" onclick="abre('rh_balanco_geral.php')" />
+    <input type="button" class="btn btn-primary btn-sm" name="Quadro de Funcionarios" id="Quadro de Funcionarios" value="Quadro de Funcionarios" onclick="abre('rh/quadro_funcionarios.php')" />
+    <input type="button" class="btn btn-danger btn-sm" name="analiseGlobal" id="analiseGlobal" value="Analise Global" onclick="abreParametro('rh/rh_balanco_analise_global.php', 'setor')" />
+   </div>
+ </div>
 
-</form>
 </body>
 </html>
