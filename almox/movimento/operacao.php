64640fba1593d907bd39c62b7407f3c86f09a85a
Tiago Correa <tiagocorrea_442@hotmail.com>
2015-03-20 13:51:49 -0400

    Alteração Almoxarifado

--- almox/movimento/operacao.php
+++ almox/movimento/operacao.php
@@ -37,7 +37,7 @@ $almox = new Almoxarifado();
 								qtd: qt,
 								valor: val},
 			function(data) {
-				console.log(data)
+
 		});
 
 	}
@@ -51,7 +51,7 @@ $almox = new Almoxarifado();
 							  tipo: tipo,
 							  data: dt
 		}, function(data){
-			console.log(data)
+
 		})
 	}
 
@@ -62,113 +62,113 @@ $almox = new Almoxarifado();
 <body>
 <div class="acao_pagina">Movimentação Almoxarifado <?php echo $_REQUEST['data'];?></div>
 <?php foreach ($almox->getMovimentos(implode('-', array_reverse(explode('/', $_GET['data'])))) as $grupo) {?>
-			<?php $valor = $almox->getValores($grupo->id, implode('-', array_reverse(explode('/', $_GET['data']))));?>
-			<div class="col-md-9">
-			<div class="label label-info col-md-12">
-			<h4><?php echo $grupo->descricao?></h4>
-			</div>
-			<table class="table table-bordered">
-			<thead>
-				<tr>
-					<th></th>
-					<th>Quantidade</th>
-					<th>Financeiro</th>
-					<td></td>
-				</tr>
-			</thead>
-			<tbody>
-				<form id="entrada<?php echo $grupo->id;?>">
-				<tr>
-					<td>Entrada</td>
-					<td><input type="text" class="form-control qtd" value="<?php echo $valor->qtdEntrada;?>" name="qtd"></td>
-					<td><input type="text" class="form-control valor" value="<?php echo $valor->valorEntrada;?>" name="valor"></td>
-					<td>
+									<?php $valor = $almox->getValores($grupo->id, implode('-', array_reverse(explode('/', $_GET['data']))));?>
+									<div class="col-md-9">
+									<div class="label label-info col-md-12">
+									<h4><?php echo $grupo->descricao?></h4>
+									</div>
+									<table class="table table-bordered">
+									<thead>
+										<tr>
+											<th></th>
+											<th>Quantidade</th>
+											<th>Financeiro</th>
+											<td></td>
+										</tr>
+									</thead>
+									<tbody>
+										<form id="entrada<?php echo $grupo->id;?>">
+										<tr>
+											<td>Entrada</td>
+											<td><input type="text" class="form-control qtd" value="<?php echo $valor->qtdEntrada;?>" name="qtd"></td>
+											<td><input type="text" class="form-control valor" value="<?php echo $valor->valorEntrada;?>" name="valor"></td>
+											<td>
 	<?php if ($valor->qtdEntrada != 0):?>
-							<button type="button" class="btn btn-danger" onclick="exclui('entrada', <?php echo $grupo->id;?>)">
-								exclui
-							</button>
+											<button id="entrada" type="button" class="btn btn-danger" onclick="exclui('entrada', <?php echo $grupo->id;?>)">
+												exclui
+											</button>
 	<?php  else :?>
-							<button type="button" class="btn btn-success" onclick="envia('entrada', <?php echo $grupo->id;?>)">
-								envia
-							</button>
+											<button id="entrada" type="button" class="btn btn-success" onclick="envia('entrada', <?php echo $grupo->id;?>)">
+												envia
+											</button>
 	<?php endif;?>
-					</td>
-				</form>
-				</tr>
-				<tr>
-				<form name="<?php echo $grupo->id;?>">
-					<td>Devolução de Entrada</td>
-					<td><input type="text" class="form-control qtd" value="<?php echo $valor->qtdDevEntrada;?>" name="qtd"></td>
-					<td><input type="text" class="form-control valor" value="<?php echo $valor->valorDevEntrada;?>" name="valor"></td>
-					<td>
+											</td>
+										</form>
+										</tr>
+										<tr>
+										<form name="devEntrada<?php echo $grupo->id;?>">
+											<td>Devolução de Entrada</td>
+											<td><input type="text" class="form-control qtd" value="<?php echo $valor->qtdDevEntrada;?>" name="qtd"></td>
+											<td><input type="text" class="form-control valor" value="<?php echo $valor->valorDevEntrada;?>" name="valor"></td>
+											<td>
 	<?php if ($valor->qtdDevEntrada != 0 && $valor->valorDevEntrada != 0):?>
-							<button type="button" class="btn btn-danger" onclick="exclui('devEntrada', <?php echo $grupo->id;?>)">
-								exclui
-							</button>
+													<button id="devEntrada" type="button" class="btn btn-danger" onclick="exclui('devEntrada', <?php echo $grupo->id;?>)">
+														exclui
+													</button>
 	<?php  else :?>
-							<button type="button" class="btn btn-success" onclick="envia('devEntrada', <?php echo $grupo->id;?>)">
-								envia
-							</button>
+													<button id="devEntrada" type="button" class="btn btn-success" onclick="envia('devEntrada', <?php echo $grupo->id;?>)">
+														envia
+													</button>
 	<?php endif;?>
-					</td>
-				</form>
-				</tr>
-				<tr>
-				<form name="<?php echo $grupo->id;?>">
-					<td>Saida</td>
-					<td><input type="text" class="form-control qtd" value="<?php echo $valor->qtdSaida;?>" name="qtd"></td>
-					<td><input type="text" class="form-control valor" value="<?php echo $valor->valorSaida;?>" name="valor"></td>
-					<td>
+											</td>
+										</form>
+										</tr>
+										<tr>
+										<form name="saida<?php echo $grupo->id;?>">
+											<td>Saida</td>
+											<td><input type="text" class="form-control qtd" value="<?php echo $valor->qtdSaida;?>" name="qtd"></td>
+											<td><input type="text" class="form-control valor" value="<?php echo $valor->valorSaida;?>" name="valor"></td>
+											<td>
 	<?php if ($valor->qtdSaida != 0 && $valor->valorSaida != 0):?>
-							<button type="button" class="btn btn-danger" onclick="exclui('saida', <?php echo $grupo->id;?>)">
-								exclui
-							</button>
+													<button id="saida" type="button" class="btn btn-danger" onclick="exclui('saida', <?php echo $grupo->id;?>)">
+														exclui
+													</button>
 	<?php  else :?>
-							<button type="button" class="btn btn-success" onclick="envia('saida', <?php echo $grupo->id;?>)">
-								envia
-							</button>
+													<button id="saida" type="button" class="btn btn-success" onclick="envia('saida', <?php echo $grupo->id;?>)">
+														envia
+													</button>
 	<?php endif;?>
-					</td>
-				</form>
-				</tr>
-				<tr>
-				<form name="<?php echo $grupo->id;?>">
-					<td>Devolução de Saida</td>
-					<td><input type="text" class="form-control qtd" value="<?php echo $valor->qtdDevSaida;?>" name="qtd"></td>
-					<td><input type="text" class="form-control valor" value="<?php echo $valor->valorDevSaida;?>" name="valor"></td>
-					<td>
+											</td>
+										</form>
+										</tr>
+										<tr>
+										<form name="devSaida<?php echo $grupo->id;?>">
+											<td>Devolução de Saida</td>
+											<td><input type="text" class="form-control qtd" value="<?php echo $valor->qtdDevSaida;?>" name="qtd"></td>
+											<td><input type="text" class="form-control valor" value="<?php echo $valor->valorDevSaida;?>" name="valor"></td>
+											<td>
 	<?php if ($valor->qtdDevSaida != 0 && $valor->valorDevEntrada != 0):?>
-							<button type="button" class="btn btn-danger" onclick="exclui('devSaida', <?php echo $grupo->id;?>)">
-								exclui
-							</button>
+													<button id="devSaida" type="button" class="btn btn-danger" onclick="exclui('devSaida', <?php echo $grupo->id;?>)">
+														exclui
+													</button>
 	<?php  else :?>
-								<button type="button" class="btn btn-success" onclick="envia('devSaida', <?php echo $grupo->id;?>)">
-									envia
-								</button>
+														<button id="devSaida" type="button" class="btn btn-success" onclick="envia('devSaida', <?php echo $grupo->id;?>)">
+															envia
+														</button>
 	<?php endif;?>
-					</td>
-				</form>
-				</tr>
-				<tr>
-				<form name="<?php echo $grupo->id;?>">
-					<td>Contagem Física</td>
-					<td><input type="text" class="form-control qtd" value="<?php echo $valor->qtdCtf;?>" name="qtd"></td>
-					<td><input type="text" class="form-control valor" value="<?php echo $valor->valorCtf;?>" name="valor"></td>
-					<td>
-						<button type="button" class="btn btn-success" onclick="envia('ctf', <?php echo $grupo->id;?>)">
-							envia
-						</button>
+											</td>
+										</form>
+										</tr>
+										<tr>
+										<form name="ctf<?php echo $grupo->id;?>">
+											<td>Contagem Física</td>
+											<td><input type="text" class="form-control qtd" value="<?php echo $valor->qtdCtf;?>" name="qtd"></td>
+											<td><input type="text" class="form-control valor" value="<?php echo $valor->valorCtf;?>" name="valor"></td>
+											<td>
+												<button id="ctf" type="button" class="btn btn-success" onclick="envia('ctf', <?php echo $grupo->id;?>)">
+													envia
+												</button>
 	<?php if ($valor->qtdCtf != 0 && $valor->qtdCtf != 0):?>
-							<button type="button" class="btn btn-danger" onclick="exclui('ctf', <?php echo $grupo->id;?>)">
-								exclui
-							</button>
+													<button id="ctf" type="button" class="btn btn-danger" onclick="exclui('ctf', <?php echo $grupo->id;?>)">
+														exclui
+													</button>
 	<?php endif;?>
 	</td>
-				</form>
-				</tr>
-					</tbody>
-				</table>
-			</div>
+										</form>
+										</tr>
+											</tbody>
+										</table>
+									</div>
 	<?php }?>
 </body>
 </html>
